8e30eeace9e04803b2390b665e029d63a025a182
owen <owen.chosen@gmail.com>
2023-07-26 15:41:30 +0100

    Done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let multiply, num1 = 2 , num2 = 31, num3 = 22, num4 = 6;
+multiply = num1 * num2;
+
+let random = Math.floor (Math.random() + 1);
+
+let mod = num3 % num4;
+
+let max = Math.max (10, 15, 20);
